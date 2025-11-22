9ac52b6c49342c715165d078af18a69e52677ccc
krzysbielsko <krzysbielsko@gmail.com>
2025-11-22 12:52:31 +0100

    Binet flops counting corrected.

--- lab1/binet.py
+++ lab1/binet.py
@@ -22,6 +22,7 @@ class BinetWrapper:
                     for k in range(matrix_a.shape[1]):
                         result[i, j] += matrix_a[i, k] * matrix_b[k, j]
                         self.flops += 2
+                    self.flops -= 1
             return result
 
 
@@ -31,22 +32,22 @@ class BinetWrapper:
         prod_1 = self.binet(a11, b11)
         prod_2 = self.binet(a12, b21)
         c1 = prod_1 + prod_2
-        self.flops += prod_1.shape[0] ** 2
+        self.flops += prod_1.shape[0] * prod_1.shape[1]
 
         prod_3 = self.binet(a11, b12)
         prod_4 = self.binet(a12, b22)
         c2 = prod_3 + prod_4
-        self.flops += prod_3.shape[0] ** 2
+        self.flops += prod_3.shape[0] * prod_3.shape[1]
 
         prod_5 = self.binet(a21, b11)
         prod_6 = self.binet(a22, b21)
         c3 = prod_5 + prod_6
-        self.flops += prod_5.shape[0] ** 2
+        self.flops += prod_5.shape[0] * prod_5.shape[1]
 
         prod_7 = self.binet(a21, b12)
         prod_8 = self.binet(a22, b22)
         c4 = prod_7 + prod_8
-        self.flops += prod_7.shape[0] ** 2
+        self.flops += prod_7.shape[0] * prod_7.shape[1]
 
         self.memory_used += c1.nbytes + c2.nbytes + c3.nbytes + c4.nbytes
         return np.vstack((np.hstack((c1, c2)), np.hstack((c3, c4))))
@@ -59,7 +60,6 @@ class BinetWrapper:
         """
         horizontal_split_point = matrix.shape[1] // 2
         vertical_split_point = matrix.shape[0] // 2
-        self.flops += 2
 
         self.memory_used += matrix.nbytes # Tworzymy 4 nowe macierze, ale w gruncie rzeczy one razem zajmują tyle pamięci co ta oryginalna
         return (matrix[:vertical_split_point, :horizontal_split_point],
